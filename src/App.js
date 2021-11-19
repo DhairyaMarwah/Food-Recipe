a60776a6c9adf97995d5e786953a1fad11573d80
Dhairya Marwah <85454844+DhairyaMarwah@users.noreply.github.com>
2021-11-18 22:28:01 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import Axios from 'axios'
+import { useState } from 'react'
+import {v4 as uuidv4} from 'uuid'
+import './App.css'
+import Recipe from './components/Recipe'
+import Alert from './components/Alert'
+const App = () => {
+    const [query, setQuery] = useState("")
+    const [recipes, setRecipes] = useState([])
+    const [alert, setAlert] = useState("")
+    const APP_ID="7e83d736"
+    const APP_KEY="bf5bf9634fa4995a15f6501e4fe37a6c"
+    const url=`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+    const getData=async()=>{
+        if(query!==""){
+        const result=await Axios.get(url)
+        if(!result.data.more)
+            return setAlert("no food with such name") 
+        setRecipes(result.data.hits)
+        console.log(result)
+        setAlert("")
+        setQuery("")
+        }
+        else{
+            setAlert("Please fill the box")
+        }
+    }
+    const onSubmit=(e)=>{
+        e.preventDefault();
+        getData()
+    }
+    const onChange=(e)=>{
+        // e.preventDefault();
+        // getData()
+        setQuery(e.target.value)
+    }
+    return (
+        <div className="App">
+            <h1>Food Recipe </h1>
+            <form action="" className="search-form"
+             onSubmit={onSubmit}
+              onChange={onChange}
+               value={query}
+               >
+                { alert!=="" && <Alert alert={alert}/>}
+                <input type="text" placeholder="Search" autoComplete="off" />
+                <input type="submit" value="search" />
+            </form>
+            <div className="recipes">
+                 {recipes!==[] && recipes.map(recipe=> 
+                    <Recipe  recipe={recipe} key={uuidv4()} /> )}
+            </div>
+            
+        </div>
+    )
 }
 
-export default App;
+export default App
